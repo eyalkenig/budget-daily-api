41b30fdf5fee89a060d5dc0145c0568fe53aa7dd
Eyal Kenig <eyalkenig@gmail.com>
2016-10-24 12:20:22 +0300

    feat(Routes):added v1 notation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  post 'authenticate', to: 'authentication#authenticate'
+  scope :path => '/v1' do
+    post 'authenticate', to: 'authentication#authenticate'
 
-  resources :expenses
-  resources :incomes
-  resources :setups
-  resources :items
+    resources :expenses
+    resources :incomes
+    resources :setups
+    resources :items
 
-  get '/summary/daily' => 'summary#daily_report'
+    get '/summary/daily' => 'summary#daily_report'
+  end
 
 end
